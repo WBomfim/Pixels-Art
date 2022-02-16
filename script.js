fab6de261ba9cce9f3c1b28af237f5f851366f69
WBomfim <willianbomfim@hotmail.com>
2022-02-16 13:16:12 -0300

    Revisão e refatoramento do código

--- script.js
+++ script.js
@@ -15,63 +15,24 @@ function addPixelSquare() {
   const pixelSquare = document.getElementById('pixel-board');
   for (let index = 1; index <= 25; index += 1) {
     const pixel = document.createElement('div');
-    pixel.style.display = 'inline-block';
-    pixel.style.backgroundColor = 'white';
-    pixel.style.border = 'solid black 1px';
-    pixel.style.width = '40px';
-    pixel.style.height = '40px';
     pixel.className = 'pixel';
-
     pixelSquare.appendChild(pixel);
   }
 }
 
 addPixelSquare();
 
-function selectedColorBlack() {
-  const colors = document.querySelectorAll('.color');
-  colors[0].className = 'color selected';
-  function selectedColor() {
-    for (let index = 0; index < colors.length; index += 1) {
-      if (colors[index].className !== 'selected') {
-        colors[index].className = 'color';
-        event.target.className = 'color selected';
-      }
-    }
-  }
-  colors[0].addEventListener('click', selectedColor);
+function selectBlackColorFirst() {
+  const colorBlack = document.querySelectorAll('.color')[0];
+  colorBlack.className = 'color selected';
 }
 
-function selectedColorRed() {
-  const colors = document.querySelectorAll('.color');
-  function selectedColor() {
-    for (let index = 0; index < colors.length; index += 1) {
-      if (colors[index].className !== 'selected') {
-        colors[index].className = 'color';
-        event.target.className = 'color selected';
-      }
-    }
-  }
-  colors[1].addEventListener('click', selectedColor);
-}
+selectBlackColorFirst();
 
-function selectedColorBlue() {
-  const colors = document.querySelectorAll('.color');
-  function selectedColor() {
-    for (let index = 0; index < colors.length; index += 1) {
-      if (colors[index].className !== 'selected') {
-        colors[index].className = 'color';
-        event.target.className = 'color selected';
-      }
-    }
-  }
-  colors[2].addEventListener('click', selectedColor);
-}
-
-function selectedColorGreen() {
+function selectedColor() {
   const colors = document.querySelectorAll('.color');
   const colorPalette = document.querySelector('#color-palette')
-  function selectedColor() {
+  function changeClass() {
     for (let index = 0; index < colors.length; index += 1) {
       if (colors[index].className !== 'selected') {
         colors[index].className = 'color';
@@ -79,18 +40,18 @@ function selectedColorGreen() {
       }
     }
   }
-  colorPalette.addEventListener('click', selectedColor);
+  colorPalette.addEventListener('click', changeClass);
 }
 
-selectedColorBlack();
-selectedColorRed();
-selectedColorBlue();
-selectedColorGreen();
+selectedColor();
 
-const pixels = document.querySelector('#pixel-board');
 function paintPixel() {
-  const colors = document.getElementsByClassName('selected');
-  event.target.style.backgroundColor = colors[0].style.backgroundColor;
+  const pixels = document.querySelector('#pixel-board');
+  function paint() {
+    const colors = document.getElementsByClassName('selected');
+    event.target.style.backgroundColor = colors[0].style.backgroundColor;
+  }
+  pixels.addEventListener('click', paint);
 }
 
-pixels.addEventListener('click', paintPixel);
+paintPixel();
