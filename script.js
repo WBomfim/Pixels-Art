4960535a4967b2a997bd46dd35d994753ce70fde
WBomfim <willianbomfim@hotmail.com>
2022-02-21 00:04:36 -0300

    Adicionado requisito 10 e refatorado o requisito 4 para otimizar o codigo

--- script.js
+++ script.js
@@ -16,12 +16,31 @@ function selectBlackColorFirst() {
   colorBlack.className = 'color selected';
 }
 
-function addPixelSquare() {
+function addPixelSquare(size) {
   const pixelSquare = document.getElementById('pixel-board');
-  for (let index = 1; index <= 25; index += 1) {
-    const pixel = document.createElement('div');
-    pixel.className = 'pixel';
-    pixelSquare.appendChild(pixel);
+  for (let index1 = 0; index1 < size; index1 += 1) {
+    const lineBoard = document.createElement('div');
+    lineBoard.className = 'lineBoard';
+    pixelSquare.appendChild(lineBoard);
+    for (let index2 = 0; index2 < size; index2 += 1) {
+      const lineBoardaa = document.querySelectorAll('.lineBoard')[index1];
+      const pixel = document.createElement('div');
+      pixel.className = 'pixel';
+      lineBoardaa.appendChild(pixel);
+    }
+  }
+}
+
+const inputBoard = document.querySelector('#board-size');
+const buttonVQV = document.querySelector('#generate-board');
+
+function boardSize() {
+  const inputValue = inputBoard.value;
+  document.querySelector('#pixel-board').innerHTML = '';
+  if (inputValue > 5 && inputValue < 50) {
+    addPixelSquare(inputValue);
+  } else if (inputValue === '') {
+    alert('Board inválido!');
   }
 }
 
@@ -62,7 +81,8 @@ function clearAllPixels() {
 
 window.onload = function loadingEvents() {
   selectBlackColorFirst();
-  addPixelSquare();
+  addPixelSquare(5);
+  buttonVQV.addEventListener('click', boardSize);
   colorPalette.addEventListener('click', colorSelected);
   pixelBoard.addEventListener('click', paintPixel);
   buttonClear.addEventListener('click', clearAllPixels);
